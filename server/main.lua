20c71de40ae5a1eacb4723bedb05adedc65304e1
VibrantRP <127861623+VibrantRP@users.noreply.github.com>
2024-10-07 19:35:17 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -6,69 +6,12 @@ math.randomseed(os.time())
 --Variables--
 -------------
 
+local randomEntranceKey = math.random(1, #Config.BlackMarketAccess.EntranceInfo.EntrancePedLocations)
+local randomEntranceLocation = Config.BlackMarketAccess.EntranceInfo.EntrancePedLocations[randomEntranceKey]
+
 ----------
 --Events--
 ----------
-
-RegisterNetEvent('blackmarket:server:UpdateMarkets', function(index, data)
-    for k, v in pairs(data.ItemsForSale) do
-        Context.MarketInfo[v.Item] = {
-            item = v.Item,
-            price = v.Price,
-            stock = v.AvailableStock,
-        }
-    end
-end)
-
-lib.callback.register('blackmarket:server:GetStockInfo', function(source, stock)
-    return Context.MarketInfo[stock]
-end)
-
-RegisterNetEvent("blackmarket:server:BuyStock", function(input, args)
-    local cost = (args.price * input)
-
-    if exports.ox_inventory:CanCarryItem(source, args.item, input) then
-        if exports.ox_inventory:RemoveItem(source, Config.MoneyItem, cost) then
-            exports.ox_inventory:AddItem(source, args.item, input)
-
-            Context.MarketInfo[args.item].stock = (Context.MarketInfo[args.item].stock - input)
-        else
-            lib.notify(source, {
-                title = "Attention",
-                description = "You don't have enough dirty money",
-                type = 'error'
-            })
-        end
-    else
-        lib.notify(source, {
-            title = "Attention",
-            description = "Inventory is full!",
-            type = 'inform'
-        })
-    end
-end)
-
-RegisterNetEvent("blackmarket:server:SellItems", function(args)
-    local itemCount = exports.ox_inventory:Search(source, 'count', args.item)
-    local payOut = itemCount * args.price
-
-    if exports.ox_inventory:CanCarryItem(source, Config.MoneyItem, payOut) then
-        if exports.ox_inventory:RemoveItem(source, args.item, itemCount) then
-            exports.ox_inventory:AddItem(source, Config.MoneyItem, payOut)
-            lib.notify(source, {
-                title = "Attention",
-                description = "You've made $"..payOut,
-                type = 'success'
-            })
-        end
-    else
-        lib.notify(source, {
-            title = "Attention",
-            description = "Inventory is full!",
-            type = 'inform'
-        })
-    end
-end)
 
 -------------
 --Callbacks--
@@ -85,4 +28,22 @@ lib.callback.register('blackmarket:server:GetPlayerJob', function(source)
     local playerJob = player.PlayerData.job.name
 
     return playerJob
+end)
+
+lib.callback.register('blackmarket:server:GetRandomEntranceLocation', function()
+
+    return randomEntranceLocation
+end)
+
+-----------
+--Threads--
+-----------
+
+CreateThread(function()
+    for k, v in pairs(Config.MarketPeds) do
+        exports.ox_inventory:RegisterShop(v.Name, {
+            name = v.Name,
+            inventory = v.ItemsForSale,
+        })
+    end
 end)
